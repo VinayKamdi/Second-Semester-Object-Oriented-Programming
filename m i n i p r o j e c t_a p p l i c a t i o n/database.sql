87c77f02285df52f9ef2d8ab1140d0f3a67e8304
VinayKamdi <118992534+VinayKamdi@users.noreply.github.com>
2022-12-09 23:59:46 +0530

    Add files via upload

---
+++ m i n i p r o j e c t_a p p l i c a t i o n/database.sql
@@ -0,0 +1,5 @@
+create database db1;
+use db1;
+create table registration (firstname varchar(30), lastname varchar(30), gender varchar(20), subs varchar(100), email varchar(30), phone varchar(20), cardname varchar(20), cardnumber varchar(20), cvv varchar(20));
+desc registration;
+select * from registration;
